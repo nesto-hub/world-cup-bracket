e6f857e4cd13fdde0ee05b39f369ac154344cbed
Ernesto Flores <epflores@stanford.edu>
2026-06-12 10:15:29 -0700

    Update page.tsx

--- app/leaderboard/[league]/page.tsx
+++ app/leaderboard/[league]/page.tsx
@@ -1,11 +1,9 @@
-"use client";
-
-import { useParams } from "next/navigation";
 import LeaderboardPage from "../page";
 
-export default function LeagueLeaderboardPage() {
-  const params = useParams();
-  const league = params.league as string;
-
-  return <LeaderboardPage leagueFromRoute={league} />;
+export default function LeagueLeaderboardPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ league?: string }>;
+}) {
+  return <LeaderboardPage searchParams={searchParams} />;
 }
